78d118ed4c849e6264f284e73d3afd4a25637f0f
MichelleBKlynn <michellestaton@gmail.com>
2016-09-14 18:59:34 -0400

    Complete answers

--- DictionaryIteration.playground/Contents.swift
+++ DictionaryIteration.playground/Contents.swift
@@ -16,7 +16,12 @@
  * The Kinks: _Low Budget_
  * The Beatles: _Sgt. Pepper's Lonely Hearts Club Band_
  */
-// write your code here
+var bestSeller = [
+"Nirvana": "Nevermind",
+"Blondie": "Parallel Lines",
+"The Kinks": "Low Budget",
+"The Beatles": "Sgt. Pepper Lonely Hearts Club Band"
+]
 
 
 
@@ -28,7 +33,12 @@
 /*: question2
  ### 2. Oops, we forgot an album! Add the Pixie's _Doolittle_ to the dictionary you created in Question 1.
  */
-// write your code here
+
+
+bestSeller["Pixie"] = "Doolittle"
+
+print(bestSeller)
+
 
 
 
@@ -40,7 +50,10 @@
 /*: question3
  ### 3. We don't care that much about The Kinks. Delete them from the dictionary you created in Question 1.
  */
-// write your code here
+bestSeller["The Kinks"] = nil
+
+print(bestSeller)
+
 
 
 
@@ -54,11 +67,25 @@
  ### 4. A band name has been defined in the constant `bandName` below. Using this constant and the dictionary you created in Question 1, print "<Band>'s top-selling album was <Album>" to the console.
  */
 let bandName = "Nirvana"
-// write your code here
+
+if let bestSeller = bestSeller[bandName] {
+print("\(bandName)'s top-selling album was \(bestSeller).")
+}
 
 
+var hawaiiGeography = [
+"Hawaii": "Kona",
+"Maui": "Kahului",
+"Oahu": "Honolulu",
+"Kauai": "Lihui"
+]
 
 
+let home = "Maui"
+
+if let hawaiiGeography = hawaiiGeography[home] {
+    print("I used to live on \(home), but not in \(hawaiiGeography), its capial.")
+}
 
 
 
@@ -66,7 +93,13 @@ let bandName = "Nirvana"
 /*: question5
  ### 5. Iterate over the dictionary you created in Question 1 and print "<Band>'s top-selling album was <Album>" for each item in the dictionary.
  */
-// write your code here
+
+for (record, artist) in bestSeller {
+    print("\(artist)'s top-selling album was \(record).")
+}
+
+
+
 
 
 
@@ -79,7 +112,7 @@ let bandName = "Nirvana"
  ### 6. What type of data structure is returned when you iterate over a dictionary?
  */
 
-
+// key value pairs
 
 
 
@@ -89,12 +122,14 @@ let bandName = "Nirvana"
 /*: question7
  ### 7. Imagine every student in every grade in a high school took an exam. Each student got a letter grade based on their test results (A, B, C, D, and F). The results were averaged together per grade. How could you represent this mapping of grades to exam average?
  */
+// Kids: student 1, student 2, student 3
+// Grade: Freshman, Sophomore, Junior, Senior
+// Score: A, B, C, D, F
 
 
+// kids in the < grade > class got an < score > average.
 
-
-
-
+// a dictionary of arrays
 
 
 /*: question8
@@ -105,7 +140,12 @@ let bandName = "Nirvana"
  * Grade 11: C
  * Grade 12: B
  */
-// write your code here
+let examResults = [
+9: "B",
+10: "A",
+11: "C",
+12: "B",
+]
 
 
 
@@ -117,11 +157,9 @@ let bandName = "Nirvana"
 /*: question9
  ### 9. Print the message "Grade <Grade> got <Score>" for each grade in the dictionary you created in Question 8.
  */
-// write your code here
-
-
-
-
+for (grade, score) in examResults {
+print("Grade \(grade) averaged \(score)s")
+}
 
 
 
@@ -129,7 +167,13 @@ let bandName = "Nirvana"
 /*: question10
  ### 10. Find which grade got the highest score! Iterate through the dictionary to find the highest score, then print the message "Grade <Grade> got the highest score with <Score>!" to the console.
  */
-// write your code here
+
+for (grade, score) in examResults {
+    if score == "A" {
+print("Grade \(grade) got the highest score with \(score)!")
+}
+}
+
 
 
 
